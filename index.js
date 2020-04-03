2807738bc36ea4c9543ee04d1db032459903f38e
bazsup <supawit.ruen@mail.kmutt.ac.th>
2020-04-03 14:37:44 +0700

    fix test did not exit after run all tests

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ app.get("/", (req, res) => {
   res.send("Hello, everyone. I want to go outside");
 });
 
+app.get("/test", (req, res) => {
+  res.send("Hello, test another endpoint.");
+});
+
 const port = 3000;
 const server = app.listen(port, () => {
   console.log(`> Application Ready on port %s.`, port);
 });
 
-module.exports = app;
+module.exports = server;
--- index.test.js
+++ index.test.js
@@ -1,10 +1,14 @@
 const request = require("supertest");
 
-const app = require("./index");
+const server = require("./index");
 
 describe("GET /", () => {
+  afterEach(async () => {
+    await server.close();
+  });
+
   it('should response text "Hello, Infra. I want to go outside."', async () => {
-    const response = await request(app).get("/");
+    const response = await request(server).get("/");
     expect(response.text).toEqual("Hello, Infra. I want to go outside");
     expect(response.statusCode).toBe(200);
   });
