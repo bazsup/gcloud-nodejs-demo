1d0ef997cedfb56655debce9cc94c33f9c166c73
bazsup <supawit.ruen@mail.kmutt.ac.th>
2020-04-12 19:31:48 +0700

    fix fail test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, Infra. Use cloudbuild.yaml.");
+  res.send("Hello, Infra. I want to go outside.");
 });
 
 const port = process.env.PORT || 8080;
