0389ffce003cdcdd45445c1fb8308feb4f143e14
bazsup <supawit.ruen@mail.kmutt.ac.th>
2020-04-16 10:57:36 +0700

    fix fail test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, Bas. I want to go outside.");
+  res.send("Hello, Infra. I want to go outside.");
 });
 
 const port = process.env.PORT || 8080;
