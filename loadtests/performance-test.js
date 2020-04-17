f6eb46f39bd0c575d2be6709b1ee5c059f5ec0f2
bazsup <supawit.ruen@mail.kmutt.ac.th>
2020-04-17 18:23:41 +0700

    add performance test script

---
+++ loadtests/performance-test.js
@@ -0,0 +1,27 @@
+import http from 'k6/http'
+import { sleep } from 'k6'
+import { Rate } from 'k6/metrics';
+
+console.log('environments: ', __ENV)
+console.log('------- APP_URL', __ENV.APP_URL)
+
+const myFailRate = new Rate('failed requests');
+
+export let options = {
+  vus: 2,
+  duration: '5s',
+  thresholds: {
+    // threshold on a custom metric
+    // - Less than 0.1% request errors.
+    'failed requests': ['rate<0.1'],
+    // threshold on a standard metric
+    // - 95th percentile response time must be below 1s.
+    'http_req_duration': ['p(95)<500'] 
+  }
+}
+
+export default () => {
+  let res = http.get(__ENV.APP_URL)
+  myFailRate.add(res.status !== 200)
+  sleep(1)
+}
